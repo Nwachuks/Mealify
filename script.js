dcce6c798d04782d291b88ee1b291020a8abb9a7
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-08-05 14:46:46 +0100

    Get and display random meal

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function searchMeal(e) {
 
                 if (data.meals === null) {
                     resultHeading.innerHTML = `<p>There are no search results. Please try another search term</p>`;
+                    mealsElement.innerHTML = '';
                 } else {
                     mealsElement.innerHTML = data.meals.map(meal =>
                         `<div class="meal">
@@ -81,10 +82,25 @@ function addMealToDOM(meal) {
             </ul>
         </div>
     </div>`;
+    singleMealElement.scrollIntoView(true);
+}
+
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsElement.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+        });
 }
 
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 mealsElement.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
         if (item.classList) {
